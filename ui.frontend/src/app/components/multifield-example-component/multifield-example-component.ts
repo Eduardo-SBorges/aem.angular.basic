dcea16614398d2e13869846b0db105ca36c25a57
Eduardo Borges <eduardo.borges@compasso.com.br>
2022-05-05 20:14:24 -0300

    update: desnecessary interface created has been removed

--- ui.frontend/src/app/components/multifield-example-component/multifield-example-component.ts
+++ ui.frontend/src/app/components/multifield-example-component/multifield-example-component.ts
@@ -1,17 +1,13 @@
 import { MapTo } from "@adobe/aem-angular-editable-components";
 import { Component, Input, OnInit } from "@angular/core";
 
-interface Multifield {
-  name: string;
-}
-
 @Component({
   selector: "app-multifield-example-component",
   templateUrl: "./multifield-example-component.html",
   styleUrls: ["./multifield-example-component.scss"],
 })
 export class MultifieldExampleComponent implements OnInit {
-  @Input() items: Multifield[];
+  @Input() items: [];
 
   constructor() {}
 
